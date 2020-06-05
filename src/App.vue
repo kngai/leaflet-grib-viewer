a381f1fb21cacb71ccb32b6313748e1aa65f0669
Kevin Ngai <kevin.ngai@canada.ca>
2020-06-05 11:31:51 -0700

    WIP leaflet-grib-tester

--- src/App.vue
+++ src/App.vue
@@ -6,51 +6,26 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h1>leaflet-grib tester</h1>
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <grib2-class-viewer></grib2-class-viewer>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Grib2ClassViewer from './components/Grib2ClassViewer';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Grib2ClassViewer,
   },
 
   data: () => ({
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
+let today = new Date().toLocaleDateString(undefined, {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit'
+})
+process.env.VUE_APP_VERSION = require('./package.json').version
+process.env.VUE_APP_BUILD_DATE = today
+
 module.exports = {
+  publicPath: process.env.PUBLIC_PATH !== null
+    ? process.env.PUBLIC_PATH // for different deployment environments of root path to web app
+    : '/',
+
   "transpileDependencies": [
     "vuetify"
   ]
